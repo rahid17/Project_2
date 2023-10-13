ce511944c0208f2d30e5fef0c72063af3087ebad
rahid17 <you@example.com>
2023-10-13 12:35:46 +0600

    update

--- lib/app/modules/splash_screen/controllers/splash_controller.dart
+++ lib/app/modules/splash_screen/controllers/splash_controller.dart
@@ -1,8 +1,9 @@
 import 'package:get/get.dart';
 import 'package:project_2/app/modules/nav_bar/views/nav_view.dart';
-import 'package:project_2/app/router/app_pages.dart';
+import 'package:project_2/data/user_data.dart';
 
 class SplashController extends GetxController{
+  var data = UserData();
 @override
   void onInit() {
     print("call");
@@ -10,12 +11,13 @@ class SplashController extends GetxController{
     super.onInit();
   }
 }
-nextPage(){
+nextPage() async {
   Future.delayed(const Duration(seconds:5) ,
   (){
     // Get.offAllNamed(Route.navScreen);
-    Get.offAllNamed(Route.navScreen);
+    // Get.offAllNamed(Route.navScreen);
   }
-  
   );
+  var userInfo = await data.getUser; 
 }
+
